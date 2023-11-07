b1d7662e55a91565ee3618d88306a6699fc2a33d
Nehayj100 <nehayj100@tamu.edu>
2023-11-07 16:12:00 -0600

    Added the feature to download the admin dashboard as a csv file

--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,7 @@ Rails.application.routes.draw do
   get 'faculty/logout', to: 'faculty#logout', as: 'faculty_logout'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
